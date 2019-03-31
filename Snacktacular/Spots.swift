b8687d3a629933019c4b9a3d7146d002caabfc22
Joey Parks <parksjd@bc.edu>
2019-03-31 15:06:28 -0400

    thru 9.7

--- Snacktacular/Spots.swift
+++ Snacktacular/Spots.swift
@@ -7,7 +7,28 @@
 //
 
 import Foundation
+import Firebase
 
 class Spots {
     var spotArray = [Spot]()
+    var db: Firestore!
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    
+    func loadData(completed: @escaping () -> ()) {
+        db.collection("spots").addSnapshotListener { (QuerySnapshot, error) in
+            guard error == nil else {
+                return completed()
+            }
+            self.spotArray = []
+            for document in QuerySnapshot!.documents {
+                let spot = Spot(dictionary: document.data())
+                spot.documentID = eodument.documentID
+                self.spotArray.appent(spot)
+            }
+            completed()
+        }
+    }
 }
